bc56c9ebdca40e25224b92fc77d37af7aec390c9
Jhosua <jhosuapvll@gmail.com>
2021-11-06 21:26:33 -0500

    Se hace el chat de emma, queda pendiente validación de formulario

--- template-parts/footer.php
+++ template-parts/footer.php
@@ -171,7 +171,127 @@
             </div>
         </div>
     </div>
+    <div class="emma" id="emman">
+        <div class="emma__primera" id="emma__primera">
+            <div alt="" id="emma__imagen" class="emma__primera-imagen"></div>
+            <div class="emma__primera-contenedor">
+                <h3 class="emma__primera-titulo">¡Hola, soy EMA!</h3>
+                <p class="emma__primera-descripcion">¿Tienes dudas o preguntas?</p>
+            </div>
+            <img src="assets/images/close-emma.svg" alt="" class="emma__primera-close">
+            <div class="emma__close" id="emma__close">
+                <img src="assets/images/close-emma-white.svg" alt="" class="emma__primera-close-white">
+            </div>
+            <div class="form__emma-contenedor" id="formulario_emma">
+                <h3 class="form__emma-titulo">¡Hola! soy EMA.</h3>
+                <p class="form__emma-descripcion">Para brindarte una mejor atención de pido me des los siguientes datos</p>
+                <form action="" class="form__emma">
+
+                    <div class="form__emma-bloque" id="form__emma-nombre">
+                        <input type="text" name="nombre" class="form__emma-input" placeholder="Nombre">
+                        <p class="form__emma-validacion">Introduzca un nombre valido</p>
+                    </div>
+
+                    <div class="form__emma-bloque" id="form__emma-celular">
+                        <input type="text" name="celular" class="form__emma-input" placeholder="Télefono">
+                        <p class="form__emma-validacion">Introduzca un telefono valido</p>
+                    </div>
+
+                    <div class="form__emma-bloque" id="form__emma-correo">
+                        <input type="email" name="correo" class="form__emma-input" placeholder="Correo">
+                        <p class="form__emma-validacion">Introduzca un correo valido</p>
+                    </div>
+
+                    <div class="form__emma-bloque" id="form__emma-terminos">
+                        <label class="formulario__label">
+                            <b class="black__light">Al hacer click acepto los </b><a href="#" class="black formulario__link">terminos y condiciones</a>
+                            <input class="formulario__checkbox" type="checkbox" name="terminos" id="terminos">
+                        </label>
+                    </div>
+
+                    <div class="form__emma-bloque">
+                        <button class="form__emma-button" type="submit">Enviar</button>
+                    </div>
+
+                </form>
+            </div>
+            <div class="emma__end form__emma-contenedor" id="emma__end">
+                <p class="emma__end-descripcion">Para continuar atendiéndote te sugiero estas opciones</p>
+                <div class="emma__end-contenedor">
+                    <a href="#" class="emma__end-links">Continuar conversación por Whatsapp</a>
+                    <a href="#" class="emma__end-links">Recibir una llamada de un asesor</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </footer>
+
+<!-- EMMA -->
+
+<script>
+
+const emmaPrimera = document.querySelector('#emma__primera');
+const emmaImagen = document.querySelector('#emma__imagen');
+const emmaCloseGrey = document.querySelector('.emma__primera-close');
+const emmaContenedor = document.querySelector('.emma__primera-contenedor');
+const emmaClose = document.querySelector('#emma__close');
+const emmaForm = document.querySelector('#formulario_emma');
+const emmaEnd = document.querySelector('#emma__end');
+
+window.onload = () =>{
+    emmaPrimera.classList.add('active');
+    const EmmaFunction = () =>{
+        emmaImagen.classList.add('active');
+        emmaContenedor.classList.add('active');
+        emmaCloseGrey.classList.add('active');
+    }
+
+    //FUNCION QUE DESACTIVA LA BIENVENIDA DESPUES DE 3 SEGUNDOS
+
+    setTimeout(() => {
+        EmmaFunction();
+    }, 3000);
+
+    emmaImagen.addEventListener('click', () =>{
+        emmaImagen.classList.add('zoom');
+        emmaClose.classList.add('active');
+        emmaForm.classList.add('active');
+        EmmaFunction();
+        if(validacion.links){
+            emmaEnd.classList.add('active');
+        }
+    });
+    emmaClose.addEventListener('click', () =>{
+        emmaImagen.classList.remove('zoom');
+        emmaClose.classList.remove('active');
+        emmaForm.classList.remove('active');
+        emmaEnd.classList.remove('active');
+        if(validacion.links){
+            emmaEnd.classList.remove('active');
+        }
+    });
+    emmaCloseGrey.addEventListener('click', () =>{
+        EmmaFunction();
+    });
+}
+
+const validacion = {
+    links: false,
+}
+
+emmaForm.addEventListener('submit', (e) =>{
+    e.preventDefault();
+    emmaForm.classList.remove('active');
+    emmaForm.classList.add('hidde-important');
+    emmaEnd.classList.add('active');
+    validacion.links = true;
+});
+
+
+</script>
+
+
+<!-- VALIDACION FORMULARIO -->
 
 <script>
     const formulario = document.getElementById('formulario');
