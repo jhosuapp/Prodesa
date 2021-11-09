42346a3291e2b95fe3e1d89ec975c6cfd43840f5
Jhosua <jhosuapvll@gmail.com>
2021-11-08 21:17:12 -0500

    Se adelanta página de pagos en linea

--- pagos-linea.php
+++ pagos-linea.php
@@ -33,7 +33,53 @@
             <h2 class="titulo__contenido"><b class="grey__light">Selecciona el tipo de pago que vas a hacer</h2>
         </div>
     </div>
+    <div class="card-pagos">
+        <div class="card-pagos__contenedor" id="container">
+            <div class="card-pagos__background"></div>
+            <p class="card-pagos__descripcion card-pagos__descripcion--hidden">Pago mensual de cuota inicial</p>
+            <div class="card-pagos__opacity">
+                <h3 class="card-pagos__descripcion"><b class="white__light">Pago mensual de cuota inicial</b> <b class="white">seleccionado</b></h3>
+                <p class="card-pagos__terminos">Acepta los terminos y condiciones y da clic en continuar</p>
+                <div class="card-pagos__contenedor-end">
+                    <img src="assets/images/pay-down.svg" alt="" class="card-pagos__icono">
+                </div>
+                <a href="#" class="card-pagos__instrucciones">Leer instrucciones de pago</a>
+            </div>
+        </div>
+        <div class="card-pagos__contenedor" id="container">
+            <div class="card-pagos__background card-pagos__background--separacion"></div>
+            <p class="card-pagos__descripcion card-pagos__descripcion--hidden">Pago de separación</p>
+            <div class="card-pagos__opacity">
+                <h3 class="card-pagos__descripcion"><b class="white__light">Pago mensual de cuota inicial</b> <b class="white">seleccionado</b></h3>
+                <p class="card-pagos__terminos card-pagos__terminos--left">Acepta los terminos y condiciones y da clic en continuar</p>
+                <div class="card-pagos__contenedor-end card-pagos__contenedor-start">
+                    <img src="assets/images/pay-down.svg" alt="" class="card-pagos__icono">
+                </div>
+                <div class="card-pagos__contenedor-end">
+                    <a href="#" class="card-pagos__instrucciones">Leer instrucciones de pago</a>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
+
+<script>
+
+function myFunctionPay() {
+    var cardPayAll, i;
+    var cardPay; 
+    console.log(cardPay)
+    var cardPayAll = document.querySelectorAll(".card-pagos__contenedor");
+    for (i = 0; i < cardPayAll.length; i++) {
+        cardPayAll[i].onclick = function() {
+            this.classList.add('active');
+        };               
+    }
+}
+myFunctionPay();
+
+</script>
+
 
 <?php 
     include 'template-parts/footer.php';
