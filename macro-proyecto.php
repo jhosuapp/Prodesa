227912a0d4472f49369cbb402a5e02869369f302
jhosuapp <jhosuapvll@gmail.com>
2021-11-14 13:01:16 -0500

    Se adelanta promo y multimedia de macro

--- macro-proyecto.php
+++ macro-proyecto.php
@@ -2,7 +2,6 @@
     include 'template-parts/header.php';
 ?>
 
-
 <div class="macro-banner">
     <img class="macro-banner__imagen" src="assets/images/macro-banner.jpg" alt="">
     <div class="macro-banner__contenedor">
@@ -57,6 +56,8 @@
     </div>
 </section>
 
+<!-- Proyecto del macro -->
+
 <section class="mi-content--background mi-content--background--white">
     <div class="titulo">
         <div class="titulo__izquierda titulo__flex"></div>
@@ -70,8 +71,8 @@
             <div class="card item card--vendido">
                 <div class="card-vendido">
                     <h2 class="card-vendido__titulo">100% vendido</h2>
-                    <button class="button button--margin">
-                        <a class="button__link">Ver avance de obra</a>
+                    <button class="button button--white button--margin">
+                        <a href="#" class="button__link button__link--white">Ver avance de obra</a>
                     </button>
                 </div>
                 <div class="card__top">
@@ -284,6 +285,116 @@
         </div>
     </div>
 </section>
+
+<!-- Contacto del macro -->
+
+<section class="mi-content--background--grey">
+    <div class="titulo">
+        <div class="titulo__contenedor titulo__flex">
+            <h2 class="titulo__contenido"><b class="white__light">¿Interesado en este</b> <b class="orange">macroproyecto?</b></h2>
+            <p class="titulo__descripcion titulo__descripcion--size"><b class="white">Contáctanos</b></p>
+            <div class="contacto-macro">
+                <button class="button button--white button--white-width button--margin">
+                    <a href="#" class="button__link button__link--white">Quiero recibir una llamada de un asesor</a>
+                </button>
+                <button class="button button--white button--white-width button--margin">
+                    <a href="#" class="button__link button__link--white">Quiero hablar con un asesor por WattsApp</a>
+                </button>
+                <button class="button button--white button--white-width button--margin">
+                    <a href="#" class="button__link button__link--white">Prefiero agendar una cita con una asesoría</a>
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- MULTIMEDIA -->
+
+<section class="mi-content--background--black mi-content--background--relative">
+    <div class="contacto-macro__cotizacion">
+        <h2 class="contacto-macro__titulo">¿Listo para comprar?</h2>
+        <a href="#" class="preguntas-pagos__boton contacto-macro__boton">Ir a preguntas frecuentes</a>
+    </div>
+    <div class="titulo">
+        <div class="titulo__izquierda titulo__flex"></div>
+        <div class="titulo__contenedor titulo__flex">
+            <h2 class="titulo__contenido"><b class="white">Multimedia</b></h2>
+        </div>
+        <div class="titulo__derecha titulo__flex"></div>
+    </div>
+    <div class="tab-macro__limit">
+        <div class="tab-macro">
+            <div class="tab-macro__controls">
+                <button class="tab-macro__button active">Video 1</button>
+                <button class="tab-macro__button">Video 2</button>
+                <button class="tab-macro__button">Video 3</button>
+            </div>
+            <div class="tab-macro__contenedor">
+                <div class="tab-macro__item">
+                    <div class="tab-macro__multimedia">
+                        <div class="tab-macro__firts-block">
+                            <p class="tab-macro__descripcion">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab a, repudiandae exercitationem odit nam nostrum quae perspiciatis neque asperiores dicta omnis explicabo ducimus architecto nobis at quisquam aperiam error iusto aut. Quo nisi blanditiis quisquam rem similique reiciendis eum? Vitae, esse repudiandae excepturi temporibus saepe sit nostrum quis ipsum delectus?</p>
+                        </div>
+                        <div class="tab-macro__second-block">
+                            <iframe width="100%" height="500" src="https://www.youtube.com/embed/f1-jV9Qwwf0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <p class="tab-macro__legales">* Texto legal de las imágenes Texto legal de las imágenes Texto legal de las imágenes Texto legal de las imágenes</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-macro__item">
+                    <div class="tab-macro__multimedia">
+                        <div class="tab-macro__firts-block">
+                            <p class="tab-macro__descripcion">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab a, repudiandae exercitationem odit nam nostrum quae perspiciatis neque asperiores dicta omnis explicabo ducimus architecto nobis at quisquam aperiam error iusto aut. Quo nisi blanditiis quisquam rem similique reiciendis eum? Vitae, esse repudiandae excepturi temporibus saepe sit nostrum quis ipsum delectus?</p>
+                        </div>
+                        <div class="tab-macro__second-block">
+                            <iframe width="100%" height="500" src="https://www.youtube.com/embed/f1-jV9Qwwf0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <p class="tab-macro__legales">* Textos legal de las imágenes Texto legal de las imágenes Texto legal de las imágenes Texto legal de las imágenes</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="tab-macro__item">
+                    <div class="tab-macro__multimedia">
+                        <div class="tab-macro__firts-block">
+                            <p class="tab-macro__descripcion">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab a, repudiandae exercitationem odit nam nostrum quae perspiciatis neque asperiores dicta omnis explicabo ducimus architecto nobis at quisquam aperiam error iusto aut. Quo nisi blanditiis quisquam rem similique reiciendis eum? Vitae, esse repudiandae excepturi temporibus saepe sit nostrum quis ipsum delectus?</p>
+                        </div>
+                        <div class="tab-macro__second-block">
+                            <iframe width="100%" height="500" src="https://www.youtube.com/embed/f1-jV9Qwwf0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                            <p class="tab-macro__legales">* Texto legal de las imágenes Texto legal de las imágenes Texto legal de las imágenes Texto legal de las imágenes</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!--NOTICIA-->
+
+<section class="mi-content--background--grey">
+    <div class="titulo">
+    </div>
+</section>
+
+<script>
+    const buttonTab = document.querySelectorAll('.tab-macro__controls button');
+
+    buttonTab.forEach((button) =>{
+        button.addEventListener('click', ()=>{
+            const parent = button.parentNode;
+            const grantParent = parent.parentNode;
+            const container = grantParent.querySelector('.tab-macro__contenedor');
+
+            const childrenList = Array.from(parent.children);
+            const index = childrenList.indexOf(button);
+            container.style.transform =  `translatex(-${index * 100}%)`;
+
+            parent.querySelectorAll('button.active').forEach(activeBtn => activeBtn.classList.remove('active'));
+
+            button.classList.add('active');
+        });
+    });
+
+</script>
 
 <?php 
     include 'template-parts/footer.php';
