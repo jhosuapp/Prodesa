f8ac34a7509875d494c00db2fb17e99e8fa7eef5
jhosuapp <jhosuapvll@gmail.com>
2021-11-13 21:49:19 -0500

    Se adelanta la parte superior de la maqueta de macro y se maquete toggle

--- macro-proyecto.php
+++ macro-proyecto.php
@@ -17,11 +17,273 @@
                 Nombre categoría
             </li>
         </ul>
-        <h2 class="macro-banner__subtitulo">Hacienda <b class="white">Alcalá</h2>
+        <h2 class="macro-banner__subtitulo">Hacienda <b class="white">Alcalá</b></h2>
+        <div class="macro-banner__ubicacion">
+            <div class="card__contenedor-ubicacion card__contenedor-ubicacion--white">
+                <img src="assets/images/ubicacion--white.svg" alt="" class="card__ubicacion-imagen">
+                <p class="card__ubicacion card__ubicacion--white">Bogotá</p>
+            </div>
+            <div class="card__contenedor-ubicacion card__contenedor-ubicacion--white">
+                <img src="assets/images/sale-de-ventas.svg" alt="" class="card__ubicacion-imagen">
+                <p class="card__ubicacion card__ubicacion--white">Sala de ventas: Km 1.8 variante Mosquera – Madrid</p>
+            </div>
+        </div>
     </div>
 </div>
 
+<!-- PROMO -->
 
+<section class="promo">
+    <div class="promo-contenedor">
+        <div class="promo-descripcion">
+            <p class="promo-descripcion__titulo">Descripción general</p>
+            <p class="promo-descripcion__contenido">El único proyecto Campestre de la zona. 
+                El proyecto con mas amenidades de la zona
+                La urbanización Hacienda Alcalá se convertirá en ejemplo de urbanismo ambiental. Se debe destacar el ambiente paisajístico creativo, saludable y contemporáneo.</p>
+        </div>
+        <div class="promo-right">
+            <img src="assets/images/gancho-grande.png" alt="" class="promo__imagen">
+            <div class="promo-botones">
+                <button class="promo-boton">
+                    <img src="assets/images/favorito.svg" alt="" class="promo-icono">
+                    <span class="promo-texto">Favorito</span>
+                </button>
+                <button class="promo-boton">
+                    <img src="assets/images/compartir.svg" alt="" class="promo-icono">
+                    <span class="promo-texto">Compartir</span>
+                </button>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mi-content--background mi-content--background--white">
+    <div class="titulo">
+        <div class="titulo__izquierda titulo__flex"></div>
+        <div class="titulo__contenedor titulo__flex">
+            <h2 class="titulo__contenido"><b class="grey__light">Proyectos del </b> <b class="orange">macroproyecto</b></h2>
+        </div>
+        <div class="titulo__derecha titulo__flex"></div>
+    </div>
+    <div class="card__contenedor-carousel">
+        <div class="owl-carousel owl-carousel-card">
+            <div class="card item card--vendido">
+                <div class="card-vendido">
+                    <h2 class="card-vendido__titulo">100% vendido</h2>
+                    <button class="button button--margin">
+                        <a class="button__link">Ver avance de obra</a>
+                    </button>
+                </div>
+                <div class="card__top">
+                    <h2 class="card__titulo">Apartamento 65m2</h2>
+                    <img src="assets/images/vis.png" alt="" class="card__logo card__logo--vis">
+                </div>
+                <div class="card__contenedor-ubicacion">
+                    <img src="assets/images/ubicacion--orange.svg" alt="" class="card__ubicacion-imagen">
+                    <p class="card__ubicacion card__ubicacion--color">Bogotá</p>
+                </div>
+                <div class="card__middle">
+                    <a class="card__background-imagen">
+                        <div class="owl-carousel owl-carousel--macro">
+                            <img src="assets/images/banner-home.png" alt="" class="card__imagen card__imagen--height">
+                            <img src="assets/images/banner-home.png" alt="" class="card__imagen card__imagen--height">
+                        </div>
+                    </a>
+                    <div class="card__bono">
+                        <p class="card__bono-descripcion">Bono navideño $5m</p>
+                    </div>
+                    <div class="card__redes">
+                        <!-- <img src="" alt="" class="card__redes-imagen"> -->
+                    </div>
+                </div>
+                <div class="card__semi-bottom">
+                    <div class="card__contenedor-descripcion">
+                        <div class="card__bloque-descripcion card__bloque-descripcion--bottom">
+                            <img src="assets/images/icon_building.svg" alt="" class="card__metraje-icono">
+                            <div class="card__contenedor-metraje">
+                                <p class="card__metraje">En construcción</p>
+                            </div>
+                        </div>
+                        <div class="card__bloque-descripcion card__bloque-descripcion--margin">
+                            <img src="assets/images/icon_area.svg" alt="" class="card__metraje-icono">
+                            <div class="card__contenedor-metraje">
+                                <p class="card__metraje">Areá privada desde 20m2</p>
+                                <p class="card__metraje">Areá construida desde 20m2</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__bottom">
+                <p class="card__descripcion card__descripcion--top">Lorem ipsum dolor sit amet consectetur adipisicing elit. Uasdasdas</p>
+                    <div class="card__precios">
+                        <p class="card__precio card__precio--color">Desde: $900.000.000</p>
+                    </div>
+                    <div class="card__bottom--flex">
+                        <button class="button card__button-top">
+                            <a class="button__link">Ver más</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="card item">
+                <div class="card__top">
+                    <h2 class="card__titulo">Apartamento 65m2</h2>
+                    <img src="assets/images/vis.png" alt="" class="card__logo card__logo--vis">
+                </div>
+                <div class="card__contenedor-ubicacion">
+                    <img src="assets/images/ubicacion--orange.svg" alt="" class="card__ubicacion-imagen">
+                    <p class="card__ubicacion card__ubicacion--color">Bogotá</p>
+                </div>
+                <div class="card__middle">
+                    <a class="card__background-imagen">
+                        <div class="owl-carousel owl-carousel--macro">
+                            <img src="assets/images/banner-home.png" alt="" class="card__imagen card__imagen--height">
+                            <img src="assets/images/banner-home.png" alt="" class="card__imagen card__imagen--height">
+                        </div>
+                    </a>
+                    <div class="card__bono">
+                        <p class="card__bono-descripcion">Bono navideño $5m</p>
+                    </div>
+                    <div class="card__redes">
+                        <!-- <img src="" alt="" class="card__redes-imagen"> -->
+                    </div>
+                </div>
+                <div class="card__semi-bottom">
+                    <div class="card__contenedor-descripcion">
+                        <div class="card__bloque-descripcion card__bloque-descripcion--bottom">
+                            <img src="assets/images/icon_building.svg" alt="" class="card__metraje-icono">
+                            <div class="card__contenedor-metraje">
+                                <p class="card__metraje">En construcción</p>
+                            </div>
+                        </div>
+                        <div class="card__bloque-descripcion card__bloque-descripcion--margin">
+                            <img src="assets/images/icon_area.svg" alt="" class="card__metraje-icono">
+                            <div class="card__contenedor-metraje">
+                                <p class="card__metraje">Areá privada desde 20m2</p>
+                                <p class="card__metraje">Areá construida desde 20m2</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__bottom">
+                <p class="card__descripcion card__descripcion--top">Lorem ipsum dolor sit amet consectetur adipisicing elit. Uasdasdas</p>
+                    <div class="card__precios">
+                        <p class="card__precio card__precio--color">Desde: $900.000.000</p>
+                    </div>
+                    <div class="card__bottom--flex">
+                        <button class="button card__button-top">
+                            <a class="button__link">Ver más</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="card item">
+                <div class="card__top">
+                    <h2 class="card__titulo">Apartamento 65m2</h2>
+                    <img src="assets/images/vis.png" alt="" class="card__logo card__logo--vis">
+                </div>
+                <div class="card__contenedor-ubicacion">
+                    <img src="assets/images/ubicacion--orange.svg" alt="" class="card__ubicacion-imagen">
+                    <p class="card__ubicacion card__ubicacion--color">Bogotá</p>
+                </div>
+                <div class="card__middle">
+                    <a class="card__background-imagen">
+                        <div class="owl-carousel owl-carousel--macro">
+                            <img src="assets/images/banner-home.png" alt="" class="card__imagen card__imagen--height">
+                            <img src="assets/images/banner-home.png" alt="" class="card__imagen card__imagen--height">
+                        </div>
+                    </a>
+                    <div class="card__bono">
+                        <p class="card__bono-descripcion">Bono navideño $5m</p>
+                    </div>
+                    <div class="card__redes">
+                        <!-- <img src="" alt="" class="card__redes-imagen"> -->
+                    </div>
+                </div>
+                <div class="card__semi-bottom">
+                    <div class="card__contenedor-descripcion">
+                        <div class="card__bloque-descripcion card__bloque-descripcion--bottom">
+                            <img src="assets/images/icon_building.svg" alt="" class="card__metraje-icono">
+                            <div class="card__contenedor-metraje">
+                                <p class="card__metraje">En construcción</p>
+                            </div>
+                        </div>
+                        <div class="card__bloque-descripcion card__bloque-descripcion--margin">
+                            <img src="assets/images/icon_area.svg" alt="" class="card__metraje-icono">
+                            <div class="card__contenedor-metraje">
+                                <p class="card__metraje">Areá privada desde 20m2</p>
+                                <p class="card__metraje">Areá construida desde 20m2</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__bottom">
+                <p class="card__descripcion card__descripcion--top">Lorem ipsum dolor sit amet consectetur adipisicing elit. Uasdasdas</p>
+                    <div class="card__precios">
+                        <p class="card__precio card__precio--color">Desde: $900.000.000</p>
+                    </div>
+                    <div class="card__bottom--flex">
+                        <button class="button card__button-top">
+                            <a class="button__link">Ver más</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+            <div class="card item">
+                <div class="card__top">
+                    <h2 class="card__titulo">Apartamento 65m2</h2>
+                    <img src="assets/images/vis.png" alt="" class="card__logo card__logo--vis">
+                </div>
+                <div class="card__contenedor-ubicacion">
+                    <img src="assets/images/ubicacion--orange.svg" alt="" class="card__ubicacion-imagen">
+                    <p class="card__ubicacion card__ubicacion--color">Bogotá</p>
+                </div>
+                <div class="card__middle">
+                    <a class="card__background-imagen">
+                        <div class="owl-carousel owl-carousel--macro">
+                            <img src="assets/images/banner-home.png" alt="" class="card__imagen card__imagen--height">
+                            <img src="assets/images/banner-home.png" alt="" class="card__imagen card__imagen--height">
+                        </div>
+                    </a>
+                    <div class="card__bono">
+                        <p class="card__bono-descripcion">Bono navideño $5m</p>
+                    </div>
+                    <div class="card__redes">
+                        <!-- <img src="" alt="" class="card__redes-imagen"> -->
+                    </div>
+                </div>
+                <div class="card__semi-bottom">
+                    <div class="card__contenedor-descripcion">
+                        <div class="card__bloque-descripcion card__bloque-descripcion--bottom">
+                            <img src="assets/images/icon_building.svg" alt="" class="card__metraje-icono">
+                            <div class="card__contenedor-metraje">
+                                <p class="card__metraje">En construcción</p>
+                            </div>
+                        </div>
+                        <div class="card__bloque-descripcion card__bloque-descripcion--margin">
+                            <img src="assets/images/icon_area.svg" alt="" class="card__metraje-icono">
+                            <div class="card__contenedor-metraje">
+                                <p class="card__metraje">Areá privada desde 20m2</p>
+                                <p class="card__metraje">Areá construida desde 20m2</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="card__bottom">
+                <p class="card__descripcion card__descripcion--top">Lorem ipsum dolor sit amet consectetur adipisicing elit. Uasdasdas</p>
+                    <div class="card__precios">
+                        <p class="card__precio card__precio--color">Desde: $900.000.000</p>
+                    </div>
+                    <div class="card__bottom--flex">
+                        <button class="button card__button-top">
+                            <a class="button__link">Ver más</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php 
     include 'template-parts/footer.php';
